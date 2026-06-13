4eb126825be98c1503780ee3136ae5bcc3c376aa
sadgirnita90-hash <sadgirnita90@gmail.com>
2026-06-13 10:50:28 +0530

    Update Solution 6.py

--- students/nita-sadgir/Solution 6.py
+++ students/nita-sadgir/Solution 6.py
@@ -3,8 +3,24 @@
 def is_leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
-# Test cases
-print(is_leap_year(2000))  # True
-print(is_leap_year(1900))  # False
-print(is_leap_year(2024))  # True
-print(is_leap_year(2023))  # False
+
+print(is_leap_year(2000))  
+print(is_leap_year(1900)) 
+print(is_leap_year(2024))  
+print(is_leap_year(2023)) 
+
+
+#proble 2
+#sum numbers
+def sum_to_n(n):
+    total = 0 
+
+    for i in range(1, n + 1):
+        total += i
+
+    return total
+
+
+print(sum_to_n(5))   
+print(sum_to_n(1))  
+print(sum_to_n(10))  
